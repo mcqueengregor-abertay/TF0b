e6a51084327655fe22501caa5ecb306f10c613b0
Kurtis Knodel <kurtisknodel@gmail.com>
2021-07-25 12:31:55 -0700

    Added mouse hiding to InputManager

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -8,6 +8,8 @@ namespace TF0b.Input
 {
 	public class InputManager : MonoBehaviour
 	{
+		public bool IsCursorVisible { get => Cursor.visible; set => Cursor.visible = value; }
+		public CursorLockMode CursorLock { get => Cursor.lockState; set => Cursor.lockState = value; }
 		public float LookSensitivity { get => lookSensitivity * sensitivityFudge; set => SetLookSensitivity(value); }
 		public Vector2 CurrentMove { get; private set; }
 		public Vector2 CurrentLook { get; private set; }
@@ -17,6 +19,23 @@ namespace TF0b.Input
 
 		void OnMove(InputValue value) => CurrentMove = value.Get<Vector2>();
 		void OnLook(InputValue value) => CurrentLook = value.Get<Vector2>();
+
+		void Start()
+		{
+			IsCursorVisible = false;
+			CursorLock = CursorLockMode.Locked;
+		}
+
+		void Update()
+		{
+			// This is for testing and is to be replaced with a menu system later.
+			if(Keyboard.current.escapeKey.wasPressedThisFrame)
+			{
+				IsCursorVisible = !IsCursorVisible;
+				if(IsCursorVisible) CursorLock = CursorLockMode.None;
+				else CursorLock = CursorLockMode.Locked;
+			}
+		}
 
 		public void SetLookSensitivity(float value)
 		{
